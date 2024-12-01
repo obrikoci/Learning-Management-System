b576d095550e9bb1a61ab972e8fdd3e3a850ccdc
Obri K <obrik@Obris-MacBook-Pro.local>
2024-12-01 02:58:57 +0100

    commit

--- server/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { refreshAccessToken } from "../controllers/authController";
-
-const router = express.Router();
-
-router.post("/refresh-token", refreshAccessToken);
-
-export default router;
-
